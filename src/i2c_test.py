97e723a6daa00cc2514c0a67acd4635feaf2a53a
Peter Chau <peter.chau-lo@peartherapeutics.com>
2022-09-25 09:54:07 -0700

    i2c testing

--- src/i2c_test.py
+++ src/i2c_test.py
@@ -4,10 +4,10 @@ import qwiic
 initialize=True
 n=2
 
-oled = qwiic.QwiicMicroOLED()
+oled = qwiic.QwiicMicroOled()
 oled.begin()
 
 #Setup OLED
 oled.clear(oled.ALL)
 oled.display()
-oled.set_font_type(1) 
+oled.set_font_type(1)
