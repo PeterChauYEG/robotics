d82c6fc1bd09c1f4f5c926cffa74cf12d8a909a5
Peter Chau <peter.chau-lo@peartherapeutics.com>
2022-09-25 18:04:28 -0700

    robot test

--- src/drone.py
+++ src/drone.py
@@ -1,35 +1,128 @@
+# TODO: show some sweet messages for staging of startup and shutdown
+# do a nicer shutdown
+# do a nicer init
+
 import asyncio
 import websockets
 import sys
+import qwiic
+import time
 
-class Drone:
-    def forward(self, speed=0.5):
-        print('forward {}'.format(speed))
 
-    def backward(self, speed=0.5):
-        print('backward {}'.format(speed))
+# monitor
+LCDWIDTH = 64
 
-    def left(self, speed=0.5):
-        print('left {}'.format(speed))
+# drivetrain
+R_MTR = 0
+L_MTR = 1
+FWD = 0
+BWD = 1
+MAX_SPEED = 255
+DEFAULT_SPEED = 100
+STOP_SPEED = 0
 
-    def right(self, speed=0.5):
-        print('right {}'.format(speed))
+
+class Monitor:
+    def __init__(self):
+        self.display = None
+
+    def init(self):
+        self.display = qwiic.QwiicMicroOled()
+
+        self.display.begin()
+        self.display.scroll_stop()
+        self.display.set_font_type(0)
+        self.clear()
+
+    def clear(self):
+        self.display.clear(self.display.PAGE)
+        self.display.clear(self.display.ALL)
+        self.display.set_cursor(0, 0)
+
+    def display_ip(self, ip):
+        self.clear()
+
+        if ip:
+            self.display.print("ip: ")
+            self.display.set_cursor(0, 8)
+            self.display.print(ip)
+        else:
+            self.display.print("No Internet!")
+
+        self.display.display()
+
+    def display_cmd(self, cmd):
+        self.clear()
+
+        self.display.print("cmd: ")
+        self.display.set_cursor(0, 8)
+        self.display.print(cmd)
+
+        self.display.display()
+
+
+class DriveTrain:
+    def __init__(self):
+        self.motorboard = None
+        self.speed = DEFAULT_SPEED
+
+    def init(self):
+        self.motorboard = qwiic.QwiicScmd()
+
+        if self.motorboard.connected == False:
+            raise Exception("Motor board not connected")
+
+        self.motorboard.begin()
+        print("Motor initialized.")
+        time.sleep(.250)
+
+        self.stop()
+
+        self.motorboard.enable()
+        print("Motor enabled")
+        time.sleep(.250)
 
     def stop(self):
-        print('stop')
+        self.motorboard.set_drive(R_MTR, FWD, STOP_SPEED)
+        self.motorboard.set_drive(L_MTR, FWD, STOP_SPEED)
+
+    def forward(self):
+        self.motorboard.set_drive(R_MTR, FWD, self.speed)
+        self.motorboard.set_drive(L_MTR, FWD, self.speed)
+
+    def backward(self):
+        self.motorboard.set_drive(R_MTR, FWD, -self.speed)
+        self.motorboard.set_drive(L_MTR, FWD, -self.speed)
+
+    def left(self):
+        self.motorboard.set_drive(R_MTR, FWD, self.speed)
+        self.motorboard.set_drive(L_MTR, FWD, -self.speed)
+
+    def right(self):
+        self.motorboard.set_drive(R_MTR, FWD, -self.speed)
+        self.motorboard.set_drive(L_MTR, FWD, self.speed)
+
+    def set_speed(self, speed):
+        self.speed = speed
+
+    def shutdown(self):
+        self.stop()
+        self.motorboard.disable()
 
 
-class Controller:
-    def __init__(self, _drone, _host):
-        self.drone = _drone
+class Drone:
+    def __init__(self, _drivetrain, _monitor, _host):
+        self.drivetrain = _drivetrain
+        self.monitor = _monitor
         self.host = _host
+
         self.websocket = None
 
     async def run(self):
         print('connecting to {}'.format(self.host))
-        return await self.connect_with_retries()
+        return await self.connect_to_server()
 
-    async def connect_with_retries(self):
+    async def connect_to_server(self):
         async with websockets.connect(self.host) as websocket:
             try:
                 print('connected')
@@ -50,17 +143,18 @@ class Controller:
 
     def msg_handler(self, msg):
         print(msg)
+        self.monitor.display_cmd(msg)
 
         if msg == 'forward':
-            self.drone.forward()
+            self.drivetrain.forward()
         elif msg == 'backward':
-            self.drone.backward()
+            self.drivetrain.backward()
         elif msg == 'left':
-            self.drone.left()
+            self.drivetrain.left()
         elif msg == 'right':
-            self.drone.right()
+            self.drivetrain.right()
         elif msg == 'stop':
-            self.drone.stop()
+            self.drivetrain.stop()
         else:
             print('unknown command')
 
@@ -69,6 +163,10 @@ class Controller:
         if self.websocket is not None:
             await self.websocket.close()
             self.websocket = None
+
+    def shutdown(self):
+        if self.drivetrain is not None:
+            self.drivetrain.shutdown()
 
 
 if __name__ == '__main__':
@@ -79,24 +177,36 @@ if __name__ == '__main__':
     if len(sys.argv) >= 1:
         host = sys.argv[1]
 
-    drone = Drone()
-    controller = Controller(drone, host)
+    drivetrain = DriveTrain()
+    drivetrain.init()
+
+    monitor = Monitor()
+    monitor.init()
+
+    drone = Drone(drivetrain, monitor, host)
 
     loop = asyncio.get_event_loop()
 
     try:
-        print('starting controller')
-        loop.run_until_complete(controller.run())
+        print('starting drone')
+        loop.run_until_complete(drone.run())
         loop.run_forever()
 
     except KeyboardInterrupt:
         print('keyboard interrupt')
 
-        if controller is not None:
-            loop.run_until_complete(controller.close_connection())
+        if drone is not None:
+            loop.run_until_complete(drone.close_connection())
+
+            if drone.drivetrain is not None:
+                drone.drivetrain.shutdown()
 
     except Exception as e:
         print('An error occurred {}'.format(e))
+
+        if drone is not None:
+            loop.run_until_complete(drone.close_connection())
+            drone.shutdown()
 
     finally:
         loop.close()
